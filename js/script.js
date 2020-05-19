eeb75a6835480142df6c03bd60b963200a4e1d79
Harut <example@mail.ru>
2020-05-19 22:45:52 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,61 @@
+'use strict';
+
+
+let number = 20;
+const leftBorder = 10;
+
+number = 5;
+console.log(number);
+
+
+/*a = 14;
+console.log(a);*/
+
+let nino;
+console.log(nino);
+
+const mimo = "mini";
+console.log(mimo);
+
+//console.log(somathing);
+
+const bool = false;
+console.log(bool);
+
+
+const Pers = {
+    name: "Arman",
+    age: 10,
+    isMarried: false
+
+};
+console.log(Pers.isMarried);
+
+let arr = ['BMW', 6, 'Saab', "Volvo"];
+console.log(arr[1]);
+
+//alert("Hello");
+
+/*const resalt = confirm("are you here?");
+console.log(resalt);
+
+const answer = prompt("Are you 18?", "18");
+console.log(typeof(answer));*/
+
+const user10 = ("Ivan");
+console.log(`Hello, ${user10}`);
+
+let incr = 10,
+    decr = 20;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+console.log(10 + 10);
+
+console.log(2 * 4 === "8");
+
+console.log(2 + 2 * 2 != 8);
